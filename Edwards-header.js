a2996117a40068c0fed82e65227424aae75aad26
EdwardsAlan <alan.edwards1103@gmail.com>
2018-12-09 19:47:39 -0500

    Update Edwards-header.js

--- Edwards-header.js
+++ Edwards-header.js
@@ -1,1 +1,12 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Alan Ewards 
+; Date:   9 December 2018
+; Description: Displays a formatted header
+;===========================================
+*/
 
+import * as headerJs from './header.js';
+
+console.log(headerJs.display("Robert", "Frost", "Week 2"));
